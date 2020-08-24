9aa56ef8a88ed2cf1850fc3a3a8b29986ffc4ecc
cristian soto <c.sotosoto3@gmail.com>
2020-08-23 21:13:16 -0400

    agregando el rest server

---
+++ server/config/config.js
@@ -0,0 +1,5 @@
+//process es un objeto global que siempre esta corriendo en toda la app node y se actualiza dependiendo del enviroment donde esta corriendo
+
+
+//PUERTO
+process.env.PORT = process.env.PORT || 3000
