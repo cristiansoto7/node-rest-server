e9cb6b45415cef0b45654c5664fb7de78f769367
cristian soto <c.sotosoto3@gmail.com>
2020-08-30 23:25:17 -0400

    variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -12,7 +12,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/bbdd-cristian';
 }else{
-    urlDB = 'mongodb+srv://cs7:7sOVrOL0nejZ7qPO@cluster0.jhc8w.mongodb.net/bbdd-cristian';
+    urlDB = process.env.MONGO_URI;//le pasamos la url de la conexion a mongo db atlas por EV. mongodb+srv://cs7:7sOVrOL0nejZ7qPO@cluster0.jhc8w.mongodb.net/bbdd-cristian
 }                                             
 
 process.env.urlDB = urlDB;
